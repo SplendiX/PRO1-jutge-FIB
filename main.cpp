7b5196e7bcc022de4110aca5ca0a98a752ea94f2
Splendix <58633085+SplendiX@users.noreply.github.com>
2020-06-23 23:52:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 using namespace std;
 
-void sumOfCubes(int a, int b, int& c) {
-		for (int i = a; i<=b; ++i)
-				c += i*i*i;
+/* 
+	Fermat Last Theorem
+ 	x³ + y³ = z³
+ 	a <= b, c <= d, a <= x <= b, c <= y <= d
+ 	
+	a	b	c	d		first 'z' natural solution
+	2	5	4	13		no solution
+	1	1	1	1		0³ + 0³ = 0³
+	0	1	0	1
+	1	100	1	100
+*/
+
+
+
+
+
+// Pre: integer 'a'
+// Post: reversed integer 'a' in expressed 'base' 
+int reverseNumber(int a, int base) {
+	int result = 0;
+	while (a > 0) {
+		result = result*base + a%base;
+		a /= base;
+	}
+	return result;
+}
+
+void sumCubes(int a, int b, int& c) {
+	for (int i = a; i<=b; ++i) {
+		c += i*i*i;
+	}
 }
 
 int main() {
